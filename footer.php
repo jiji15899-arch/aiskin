218b6f36bb61a210a36d89e2b50f5db89fda6061
jiji15899-arch <jiji15899@gmail.com>
2026-01-19 15:44:32 +0900

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * 정부지원금 찾기 테마 Footer
+ * 
+ * @package Subsidy_Finder
+ */
+?>
+
+<div class="support-finder-footer">
+    <div class="support-finder-footer-content">
+        <a class="support-finder-footer-request-button" href="https://aros100.com/promotions/42277">
+            💰 누락된 지원금 요청하기
+        </a>
+        
+        <div class="support-finder-footer-info">
+            <p class="creator">제작자 : 아로스</p>
+            <p class="homepage">
+                홈페이지 : <a href="https://aros100.com/" rel="noopener noreferrer" target="_blank">바로가기</a>
+            </p>
+            <p class="copyright">Copyrights © <?php echo date('Y'); ?> All Rights Reserved by (주)아백</p>
+        </div>
+    </div>
+</div>
+
+<?php wp_footer(); ?>
+</body>
+</html>
