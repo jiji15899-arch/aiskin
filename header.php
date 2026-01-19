356e543ef5dcecab19829ca02776f972dcc5d653
jiji15899-arch <jiji15899@gmail.com>
2026-01-19 15:45:00 +0900

    Create header.php

---
+++ header.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * 정부지원금 찾기 테마 Header
+ * 
+ * @package Subsidy_Finder
+ */
+?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<?php wp_body_open(); ?>
